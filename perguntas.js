6b48ae72788a1e91a1d13dd616d7804dd6a0dbf4
maarianee <142243147+maarianee@users.noreply.github.com>
2024-11-12 09:51:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+  
+    'Literatura',
+    'Quem são outer banks??',
+    'Outer Banks acompanha um grupo de adolescentes da Carolina do Norte, mais conhecidos como Pogues'
+)
